60231ade49d65b7326545262c79cb6635ebed93f
Alice Sommer <alice.j.sommer@gmail.com>
2020-05-04 13:09:05 +0200

    update file paths

--- ASV_phylotree.R
+++ ASV_phylotree.R
@@ -2,15 +2,19 @@ library(phyloseq); packageVersion("phyloseq")
 library(ggplot2); packageVersion("ggplot2")
 library(ggtree)
 
+###############################################################################
+
 # set working directory
-setwd('/Volumes/GoogleDrive/My\ Drive/DOCTORATE/Thesis')
+setwd('/Users/alicesommer/Desktop/Bureau/DOCTORATE/data_pipeline_microbiome')
 
 # load microbiome data
-ASV_table <- readRDS('KORA\ DATA/Microbiome_data/dada2output/dada2output2020/seqtab2020.rds')
-ASV_table_filtered <- readRDS('KORA\ DATA/Microbiome_data/dada2output/dada2output2020/seqtab2020_filtered.rds')
-taxon_assign <- readRDS('KORA\ DATA/Microbiome_data/dada2output/dada2output2020/taxa2020.rds')
+ASV_table <- readRDS('dada2output/seqtab2020.rds')
+ASV_table_filtered <- readRDS('dada2output/seqtab2020_filtered.rds')
+taxon_assign <- readRDS('dada2output/taxa2020.rds')
 # load phylogenetic information
-load("KORA\ DATA/Microbiome_data/dada2output/dada2output2020/phylotree2020.phy")
+load("dada2output/phylotree2020.phy")
+
+###############################################################################
 
 ps_big <- phyloseq(otu_table(ASV_table, taxa_are_rows = FALSE), ## small OTU TABLE
                    tax_table(taxon_assign),
@@ -20,9 +24,9 @@ ps_big
 tree_big <- ggtree(ps_big, aes(color=Phylum), branch.length='none', layout = "circular") 
 tree_big <- tree_big + theme(legend.position="bottom")
 
-# save(tree_big, file='/Volumes/GoogleDrive/My\ Drive/DOCTORATE/Thesis/Microbiome\ 2020/2.\ February/big_phylo_tree20.rds') 
+# save(tree_big, file='big_phylo_tree20.rds') 
 
-# ggsave(file = '/Volumes/GoogleDrive/My\ Drive/DOCTORATE/Thesis/Microbiome\ 2020/2.\ February/big_phylo_plot20.jpeg',
+# ggsave(file = 'big_phylo_plot20.jpeg',
 #               tree_big,
 #               dpi=300,
 #               width = 300,
@@ -37,9 +41,9 @@ ps_small
 tree_small <- ggtree(ps_small, aes(color=Phylum), branch.length='none', layout = "circular") 
 tree_small <- tree_small + theme(legend.position="bottom")
 
-# save(tree_small, file='/Volumes/GoogleDrive/My\ Drive/DOCTORATE/Thesis/Microbiome\ 2020/2.\ February/small_phylo_tree20.rds') 
+# save(tree_small, file='small_phylo_tree20.rds') 
 
-# ggsave(file = '/Volumes/GoogleDrive/My\ Drive/DOCTORATE/Thesis/Microbiome\ 2020/2.\ February/small_phylo_plot20.jpeg',
+# ggsave(file = 'figures/small_phylo_plot20.jpeg',
 #               tree_small,
 #               dpi=300,
 #               width = 300,
