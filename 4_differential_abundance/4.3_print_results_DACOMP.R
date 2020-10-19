74f0e11d98aa6b3520f0e3c82388a08838e290e3
Alice Sommer <alice.j.sommer@gmail.com>
2020-10-19 12:06:18 +0200

    print tree

--- 4_differential_abundance/4.3_print_results_DACOMP.R
+++ 4_differential_abundance/4.3_print_results_DACOMP.R
@@ -1,4 +1,9 @@
 library(phyloseq)
+library(ggplot2)
+library(ggtree)
+library(pals)
+library(DECIPHER)
+library(phangorn)
 
 setwd('/Users/alicesommer/Desktop/DACOMP_cluster/')
 rank_names <- c( "ASV" , "Species" , "Genus"  , "Family" , "Order"  , "Class"  , "Phylum")
@@ -17,6 +22,52 @@ for (d in 1:7){
   ps <- readRDS(paste0("ps_filt_", d, ".rds"))
   print(unname(tax_table(ps)[which(condition),]))
 }
+
+
+#### Plot tree ####
+load('/Users/alicesommer/Desktop/Bureau/DOCTORATE/data_pipeline_microbiome/dada2output/phylotree2020.RData')
+# load microbiome data
+ps_smoke <- readRDS(paste0("/Users/alicesommer/Desktop/DACOMP_cluster/ps_filt_3.rds"))
+
+ps_smoke_tree <- merge_phyloseq(ps_smoke, phy_tree(tGTR$tree))
+
+tip_labels <- unname(tax_table(ps_smoke)[,"Genus"])
+tip_labels[is.na(tip_labels)] <- paste0("Order_", unname(tax_table(ps_smoke)[is.na(tip_labels),"Order"]))
+
+# for effect size
+tip_effect <- data.frame(label = rownames(tax_table(ps_smoke)), 
+                         effect = abs(dacomp_results$raw_test_stat[dacomp_results$rank == "Genus"]),
+                         p_value_adj = dacomp_results$p_value_adj[dacomp_results$rank == "Genus"],
+                         tip_labels = tip_labels)
+
+# # for p-value
+# tip_p_value_heat <- data.frame(p_value = dacomp_results$p_value_adj[dacomp_results$rank == "Genus"])
+# rownames(tip_p_value) <- rownames(tax_table(ps_smoke))
+
+tree_smoke <- ggtree(ps_smoke_tree, color = 'grey', 
+                     # ladderize = FALSE,
+                     branch.length='none', 
+                     layout = "circular") 
+                     
+tree_smoke_effect <- tree_smoke %<+% tip_effect +
+                    geom_tippoint(aes(color=Phylum, size = p_value_adj), alpha=0.7) +
+                    scale_size(trans = 'reverse') +
+                      # aes(color=Phylum)
+                      # scale_color_viridis_d()
+                    scale_color_brewer(palette = "Set1") +
+                    # scale_color_manual(values=phylcol, labels=levels(tree_smoke$data$Phylum))
+                    geom_tiplab(aes(label = tip_labels, color = Phylum), size = 3, offset = 2) +
+                    xlim(0, 50) 
+                    # theme(legend.position="bottom")
+
+# p <- gheatmap(tree_smoke_effect, tip_p_value, offset=.5, width=.03) 
+
+ggsave(tree_smoke_effect, file = "/Users/alicesommer/Desktop/phylo_tree_DA_smoke.png",
+              dpi=300,
+              width = 300,
+              height = 350,
+              units = "mm")
+ 
 
 #############
 # POLLUTION #
